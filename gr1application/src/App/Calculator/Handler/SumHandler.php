f69b3b843daad88edda82e486b476784c07a7812
Nikita Mikhailov <kpoleg@list.ru>
2021-09-17 16:45:48 +0300

    Обработчик суммы

---
+++ gr1application/src/App/Calculator/Handler/SumHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Calculator\Handler;
+
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class SumHandler implements RequestHandlerInterface
+{
+
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $target = $request->getQueryParams();
+        $result = array_sum($target);
+        return new JsonResponse(['answer' => $result]);
+    }
+}
